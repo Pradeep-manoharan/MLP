d89643222d132c2cf16fcb057e3812a9fd8812e1
pradeep manoharan <mpradeep.kalam@gmail.com>
2024-02-19 21:04:59 +0530

    MLP || Day-1

--- MlP with Interconnected the hidden layer.py
+++ MlP with Interconnected the hidden layer.py
@@ -26,36 +26,57 @@ test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, s
 
 # Model Building
 
-class mlp(nn.Module):
+class MLP(nn.Module):
     def __init__(self, num_inputs, num_hidden, num_classes):
-        super(mlp,self).__init__()
+        super().__init__()
         self.num_inputs = num_inputs
         self.num_hidden = num_hidden
         self.num_classes = num_classes
 
-        self.L1 = nn.Linear(num_inputs, num_hidden)
-        self.relu = nn.ReLU()
-        self.L2 = nn.Linear(num_hidden, num_classes)
+        # Layers of input-hidden connection
+        self.fc1 = nn.Linear(num_inputs, num_hidden)
+
+        # Layers for interconnection within Hidden_size
+        self.fc2 = nn.Linear(num_hidden, num_hidden)
+        self.fc3 = nn.Linear(num_hidden, num_hidden)
+        self.fc4 = nn.Linear(num_hidden, num_hidden)
+
+        # Layer for hidden-output connection
+        self.fc5 = nn.Linear(num_hidden, num_classes)
+
+        # Action function
+
+        self.activation = nn.ReLU()
+        self.output_activation = nn.Sigmoid()
 
     def forward(self, x):
-        out = self.L1(x)
+        # Input-Hidden Connection
+        x = self.fc1(x)
+        x = self.activation(x)
 
-        out = self.relu(out)
-        out = self.L2(out)
+        # Interconnection within Hidden Layer
+
+        x1 = self.fc2(x)
+        x2 = self.fc3(x)
+        x3 = self.fc4(x)
+
+        hidden = x1 + x2 + x3
+
+        hidden = self.activation(hidden)
+
+        # Hidden = self.output
+
+        out = self.fc5(hidden)
 
         return out
 
 
-Model = mlp(num_inputs, num_hidden, classes)
-
+Model = MLP(num_inputs, num_hidden, classes)
 total_params = sum(p.numel() for p in Model.parameters())
 print(f"Total trainable parameters: {total_params}")
 
-
-
-
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(Model.parameters(),learning_rate)
+optimizer = torch.optim.Adam(Model.parameters(), learning_rate)
 
 # Training Model
 
@@ -64,7 +85,6 @@ n_step = len(train_loader)
 for i in range(epochs):
 
     for j, (image, label) in enumerate(train_loader):
-        img = image.shape
         image = image.reshape(-1, 28 * 28)  # Flatting
 
         # Forward Pass
@@ -78,29 +98,23 @@ for i in range(epochs):
         loss.backward()
         optimizer.step()
 
-        if(j+1) % 100 == 0:
-            print(f"Epochs [{i+1}/{epochs}], Step [{j+1}/{n_step}], loss:{loss.item()}")
+        if (j + 1) % 100 == 0:
+            print(f"Epochs [{i + 1}/{epochs}], Step [{j + 1}/{n_step}], loss:{loss.item()}")
 
 with torch.no_grad():
     n_correct = 0
     n_sample = 0
 
-    for image,label in test_loader:
-
-        image = image.reshape(-1, 28*28)
+    for image, label in test_loader:
+        image = image.reshape(-1, 28 * 28)
         output = Model(image)
 
         # Value_Index
 
-        _, prediction = torch.max(output,-1)
+        _, prediction = torch.max(output, -1)
         n_sample += label.shape[0]
         n_correct += (prediction == label).sum().item()
 
 accuracy = 100 * n_correct / n_sample
 
 print(f"Accuracy = {accuracy}")
-
-#1st Accuracy = 68.66
-#Total trainable parameters: 2395
-
-
